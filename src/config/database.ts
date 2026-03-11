21f17e814e73a89cd39967b645144251b4106a2e
alhaz-431 <mohammadalhaz431l@gmail.com>
2026-03-11 13:18:28 +0600

    Fix SSL for local and production

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,9 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   connectionTimeoutMillis: 10000,
   idleTimeoutMillis: 30000,
 });
